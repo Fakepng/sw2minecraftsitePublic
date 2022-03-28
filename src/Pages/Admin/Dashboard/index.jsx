037fbae2c3fa2361e0575959091c829076fffadc
Fakepng <krit0kasemtewin@gmail.com>
2022-03-28 13:39:31 +0700

    Add: Dashboard to navigate in admin page

--- src/Pages/Admin/Dashboard/index.jsx
+++ src/Pages/Admin/Dashboard/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import  { NavLink } from 'react-router-dom'
 import './Dashboard.css'
 
 const Dashboard = () => {
+
   return (
-    <div className="Dashboard">Dashboard</div>
+    <div className="Dashboard">
+      <p>Dashboard</p>
+      <NavLink to='/admin/register' >Register</NavLink>
+      <NavLink to='/admin/event' >Event</NavLink>
+      <NavLink to='/admin' >Back</NavLink>
+    </div>
   )
 }
 
