967f9b34f024fa86684927b2798e8bf0970a043e
Fakepng <krit0kasemtewin@gmail.com>
2022-04-09 02:00:07 +0700

    Multiple change read Discription
    - Fix: Token check to work properly
    - Add: Listen to user type "admin" to navigate to /admin

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,35 @@ import Team from './Pages/Team'
 import Register from './Pages/Register'
 import Contact from './Pages/Contact'
 import FourOFour from './Pages/FourOFour'
-import Render from './Pages/Render'
+import Render from './Pages/Admin/Render'
 import SignIn from './Pages/Admin'
 import Dashboard from './Pages/Admin/Dashboard'
 import AdminRegister from './Pages/Admin/Register'
 import AdminRepass from './Pages/Admin/Repass'
 import EventQuery from './Pages/Admin/Event'
-import { useJwt } from "react-jwt";
+import { isExpired } from "react-jwt";
+import  { useNavigate } from 'react-router-dom'
 
 const App = () => {
   const token = localStorage.getItem("accessToken");
-  const { isExpired } = useJwt(token);
+  const isTokenExpired = isExpired(token);
 
-  if (!token && isExpired) {
+  const navigate = useNavigate();
+
+  let buffer = [];
+
+  document.addEventListener('keydown', event => {
+    const charList = 'admin';
+    const key = event.key.toLowerCase();
+    if (charList.indexOf(key) === -1) {buffer = []; return};
+    buffer.push(key);
+    if (buffer.join('') === charList) {
+      buffer = [];
+      navigate('/admin');
+    }
+  });
+
+  if (!token || isTokenExpired) {
     return (
       <>
         <Navbar />
@@ -49,22 +65,22 @@ const App = () => {
     <>
       <AdminNav />
       <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/skin" element={<Skin />} />
-          <Route path="/event" element={<Event />} />
-          <Route path="/event/:id" element={<Games />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/render" element={<Render />} />
-          <Route path="/admin" element={<SignIn />} />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/register" element={<AdminRegister />} />
-          <Route path="/admin/repass" element={<AdminRepass />} />
-          <Route path="/admin/event" element={<EventQuery />} />
-          <Route path="*" element={<FourOFour />} status={404} />
-        </Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/skin" element={<Skin />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/event/:id" element={<Games />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/admin/render" element={<Render />} />
+        <Route path="/admin" element={<SignIn />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/register" element={<AdminRegister />} />
+        <Route path="/admin/repass" element={<AdminRepass />} />
+        <Route path="/admin/event" element={<EventQuery />} />
+        <Route path="*" element={<FourOFour />} status={404} />
+      </Routes>
       <Footer />
     </>
   )
